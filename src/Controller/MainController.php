b6b56c91654be195c26ee5eb9189a3fc57cb0a36
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-04-19 15:27:48 +0200

    Create Homepage template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting options for Starship parts!');
+        $starshipCount = 457;
+
+        $myShip = [
+            'name' => 'Stupid LeafyCruiser (COCO-0001)',
+            'class' => 'Garden',
+            'captain' => 'Nadin Jardin',
+            'status' => 'under construction',
+        ];
+
+        // Response 2: using Twig
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
+
+        // Response 1: with raw html
+        //return new Response('<strong>Starshop</strong>: your monopoly-busting options for Starship parts!');
     }
 }
