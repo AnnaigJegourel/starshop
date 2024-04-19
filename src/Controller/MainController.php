dc8791f15501de46e5958c1e4de21123581d43ed
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-04-19 12:33:54 +0200

    first fix with CS fixer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,11 +5,11 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController {
-
+class MainController
+{
     #[Route('/')]
-    public function homepage(): Response {
-
+    public function homepage(): Response
+    {
         return new Response('<strong>Starshop</strong>: your monopoly-busting options for Starship parts!');
     }
 }
