8540e4eb973a78799e4c06810ec8612c7a19e38c
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-04-30 14:07:12 +0200

    Stimulus: Add promise for animation (bug??)

--- assets/controllers/closeable_controller.js
+++ assets/controllers/closeable_controller.js
@@ -1,8 +1,20 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    close() {
+    async close() {
+        //d'abord, réduire la largeur de l'élément
         this.element.width = '0';
+
+        //ensuite, attendre que l'animation soit finie
+        await this.#waitForAnimation();
+
+        //enfin, faire disparaître l'élément
         this.element.remove();
+    }
+
+    #waitForAnimation() {
+        return Promise.all(
+            this.element.getAnimations().map((animation) => animation.finished),
+        );
     }
 }
